1c6d515358ccbb240184ddd25583b22d2206b96c
KobeVK <skvaknin@gmail.com>
2023-01-21 01:08:45 +0000

    shrinking terraform build

--- main.tf
+++ main.tf
@@ -2,21 +2,9 @@ provider "aws" {
   region     = var.aws_region
 }
 
-resource "aws_security_group" "sg" {
-  name        = "sec_group"
-  description = "Allow access from service two"
-}
-
-#Create a new EC2 launch configuration
-resource "aws_instance" "ec2_public" {
-  ami                         = var.ami_id
-  instance_type               = var.instance_type
-  key_name                    = var.key_name
-  security_groups             = aws_security_group.sg.name
-  subnet_id                   = var.subnet_id
-  tags                        = var.tag_name
-  associate_public_ip_address = true
-  lifecycle {
-    create_before_destroy = true
-  }
+resource "aws_instance" "example" {
+  ami           = "ami-0a89a7563fc68be84"
+  instance_type = "t2.micro"
+  key_name      = "mac_23"
+  vpc_security_group_ids =  ["sg-0fdca7d4d5179465b"]
 }
