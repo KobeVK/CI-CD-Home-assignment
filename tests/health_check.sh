01261cb369a6fe436dcbad3cf39f448557b2c849
KobeVK <skvaknin@gmail.com>
2023-01-22 01:14:53 +0000

    remove exit1

--- tests/health_check.sh
+++ tests/health_check.sh
@@ -7,7 +7,7 @@ IP=$1
 
 STATUSCODE=$(curl --silent -I http://${IP}:8000/ | head -n 1|cut -d$' ' -f2)
 if [[ test $STATUSCODE -ne 200 ]]; then
-    exit 1
+    #exit 1
     echo "fail"
     # send email
 else
