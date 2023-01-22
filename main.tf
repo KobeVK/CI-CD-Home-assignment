d2b4a7b15df38e56cc964ab58b161c66ceb957cb
KobeVK <skvaknin@gmail.com>
2023-01-22 21:29:07 +0000

    fixing destory

--- main.tf
+++ main.tf
@@ -11,11 +11,12 @@ resource "aws_instance" "web_app" {
   instance_type = "t2.micro"
   key_name      = "mac_23"
   vpc_security_group_ids =  ["sg-0fdca7d4d5179465b"]
+
+  lifecycle {
+    prevent_destroy = "${var.prevent_destroy}"
+  }
 }
 
-lifecycle {
-  prevent_destroy = "${var.prevent_destroy}"
-}
 
 output "web_app_access_ip" {  
   value = aws_instance.web_app.public_ip
